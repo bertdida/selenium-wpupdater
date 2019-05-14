5007e0a0b7c8a8f1f5167fec93e7442bc5490447
Herbert Verdida <hjverdida@gmail.com>
2019-05-14 13:24:31 +0800

    Docs: added comment

--- wpupdater.py
+++ wpupdater.py
@@ -50,7 +50,7 @@ for account in accounts:
     assert 'Dashboard' in driver.title
 
     dashboard_url = driver.current_url
-    dashboard_url = dashboard_url[:dashboard_url.find('?')]
+    dashboard_url = dashboard_url[:dashboard_url.find('?')]  # remove query string
 
     updates_url = '{}/update-core.php'.format(dashboard_url.rstrip('/'))
     driver.get(updates_url)
